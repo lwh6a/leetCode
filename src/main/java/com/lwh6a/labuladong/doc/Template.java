678052024c5c6fc3202404f5ea5b10a7656b65d0
wenhao.liu <wenhao.liu@opay-inc.com>
2022-01-20 15:21:10 +0800

    算法小抄

---
+++ src/main/java/com/lwh6a/labuladong/doc/Template.java
@@ -0,0 +1,61 @@
+package com.lwh6a.labuladong.doc;
+
+/**
+ * 算法和数据结构的框架思维
+ *
+ * @author <a href="mailto:liuwenhao@fenxiangbuy.com">刘文豪</a>
+ * @since 2021/4/30
+ */
+
+public class Template {
+
+    // 数组遍历
+    void traverse(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            // 迭代访问 arr[i]
+        }
+    }
+
+    // 链表遍历
+    void traverse(ListNode head) {
+        for (ListNode p = head; p != null; p = p.next) {
+            // 迭代访问 p.val
+        }
+    }
+
+    // 递归访问
+    void traverseRecursive(ListNode head) {
+        // 业务处理, head.val
+        traverseRecursive(head.next);
+    }
+
+    // 二叉树遍历, 典型的非线性递归遍历结构
+    void traverseTree(TreeNode root) {
+        traverseTree(root.left);
+        traverseTree(root.right);
+    }
+
+    // N叉树遍历
+    void traverseTres(TreeNodes root) {
+        for (TreeNodes child : root.children) {
+            traverseTres(child);
+        }
+    }
+
+}
+
+class ListNode {
+    int val;
+    ListNode next;
+}
+
+class TreeNode {
+    int val;
+    TreeNode left, right;
+}
+
+// 基本的N叉树节点
+class TreeNodes {
+    int val;
+    TreeNodes[] children;
+}
---
+++ src/main/java/com/lwh6a/labuladong/labuladong的算法小抄.md
@@ -0,0 +1,2 @@
+labuladong的算法小抄
+地址: https://labuladong.gitee.io/algo/
