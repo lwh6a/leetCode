b24125a4cdd3b41a2b517458c2d24a63092b72d7
wenhao.liu <wenhao.liu@opay-inc.com>
2023-03-27 21:22:37 +0800

    2023-03-27练习

---
+++ src/main/java/com/lwh6a/leetcode/other/Solution455.java
@@ -0,0 +1,30 @@
+package com.lwh6a.leetcode.other;
+
+import java.util.Arrays;
+
+/**
+ * 分发饼干
+ *
+ * @author lwh6a
+ * @since 2023/3/27
+ */
+
+public class Solution455 {
+
+
+    public int findContentChildren(int[] g, int[] s) {
+        Arrays.sort(g);
+        Arrays.sort(s);
+        int res = 0;
+        for (int i = 0, j = 0; i < g.length && j < s.length; i++,j++) {
+
+            while (j < s.length && g[i] > s[j]){
+                j++;
+            }
+            if (j < s.length) {
+                res++;
+            }
+        }
+        return res;
+    }
+}
---
+++ src/main/java/com/lwh6a/leetcode/other/Solution506.java
@@ -0,0 +1,36 @@
+package com.lwh6a.leetcode.other;
+
+import java.util.TreeMap;
+
+/**
+ * 相对名次
+ *
+ * @author lwh6a
+ * @since 2023/3/27
+ */
+
+public class Solution506 {
+
+    public String[] findRelativeRanks(int[] score) {
+        TreeMap<Integer, Integer> treeMap = new TreeMap<>((o1, o2) -> o2 - o1);
+        for (int i = 0; i < score.length; i++) {
+            treeMap.put(score[i], i);
+        }
+        String[] res = new String[score.length];
+        int x = 1;
+        for (Integer k : treeMap.keySet()) {
+            int v = treeMap.get(k);
+            if (x == 1) {
+                res[v] = "Gold Medal";
+            }else if (x == 2) {
+                res[v] = "Silver Medal";
+            }else if (x == 3) {
+                res[v] = "Bronze Medal";
+            }else {
+                res[v] = x + 1 + "";
+            }
+            x++;
+        }
+        return res;
+    }
+}
